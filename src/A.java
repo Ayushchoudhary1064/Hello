c9250b31a72a164461b2aeb37c15866ad8e09039
Ayush choudhary <contact.choudharyayush@gmail.com>
2024-06-08 19:23:02 +0530

    hello

---
+++ src/A.java
@@ -0,0 +1,7 @@
+public class A {
+      static int a=20;
+        static int display(){
+           return 200;
+       }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    int a=10;
+    static int b=20;
+    int display(){
+        return 108;
+    }
+    static int display1(){
+        return 200;
+    }
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
+        Main a1=new Main();
+        System.out.println(a1.a);
+        System.out.println(a1.display());
+        System.out.println(Main.b);
+        System.out.println(Main.display1());
+
     }
 }
